7b821aa12b643f4027c3fe29be105fc50f0f9894
fg222ak <fg222ak@student.lnu.se>
2019-01-17 11:32:56 +0100

    Commit boilerplate

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app: express.Application = express()
+
+app.get('/api', (req, res) => {
+  const obj = [{ 1: '1' }, { 2: '2' }, { 3: '3' }]
+  res.json(obj)
+})
+
+const port = 3001
+app.listen(port, () => {
+  console.log(`Server started on ${port}`)
+})
